930edb3760d28969f71587f5fb89bcde141f082f
monishkajethanii <monishkayesim@gmail.com>
2025-06-08 03:07:37 +0530

    cors origin update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://task-frontend-seven.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
